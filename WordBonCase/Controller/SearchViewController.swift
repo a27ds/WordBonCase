bf82340559595b1a3f21199a650a83d50c055f2f
a27ds <andy27ds@mac.com>
2019-01-16 15:54:34 +0100

    done alot.. Forgot to commit... :(

--- WordBonCase/Controller/SearchViewController.swift
+++ WordBonCase/Controller/SearchViewController.swift
@@ -8,26 +8,53 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
     
-    @IBOutlet weak var textViewTest: UITextView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     var words: [String] = []
-
+    var filtredWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        textViewTest.text = words[1]
+        tableView.dataSource = self
+        searchBar.delegate = self
+        filtredWords = words
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filtredWords.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "wordCell")!
+        let text = filtredWords[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //This line of code i didn't write, i understand in someway it's meaning but i think i need an explanation from you. it's so cramped up! Did a own filter function in the end (I don't use it)
+        filtredWords = searchText.isEmpty ? words : words.filter { (item: String) -> Bool in
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func filterContentForSearchText(searchText: String) {
+        filtredWords = words.filter { item in
+            return item.lowercased().contains(searchText.lowercased())
+        }
+    }
 }
--- WordBonCase/Controller/ViewController.swift
+++ WordBonCase/Controller/ViewController.swift
@@ -10,41 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     let url = URL(string:"http://runeberg.org/words/ss100.txt")!
-    var textToDisplay = ""
-    var lines : [String] = []
+    var textFile = ""
+    var words : [String] = []
     
     override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(downloadComplete(_:)), name: Notification.Name(rawValue: "downloadComplete"), object: nil)
-        downloadTextFile(url: url)
+    }
+    
+    override func viewDidLoad() {
+        downloadData()
+    }
+    
+    func downloadData() {
+        let task = URLSession.shared.dataTask(with:url) { (data, response, error) in
+            if error != nil {
+                print(error!)
+            }
+            else {
+                self.textFile = String(decoding: data!, as: UTF8.self)
+                // Bug: Couldn't encode to isoLatin1 so no ÅÄÖ right now
+                self.words = self.textFile.components(separatedBy: "\n")
+                // I tried alot to make the download async and failed everytime.. I tried with a combination of DispatchQueue and completionHandler but i couldn't get it to wait until everything where downloaded and converted, so I made an Notification to switch VC when it was downloaded.
+                DispatchQueue.main.async {
+                    NotificationCenter.default.post(name: Notification.Name(rawValue: "downloadComplete"), object: nil)
+                }
+            }
+        }
+        task.resume()
     }
     
     @objc func downloadComplete(_ notification: Notification) {
-         lines = textToDisplay.components(separatedBy: "\n")
-        print(lines.count)
         self.performSegue(withIdentifier: "search", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "search") {
             let vc = segue.destination as! SearchViewController
-            vc.words = lines
+            vc.words = words
         }
-    }
-    
-    func downloadTextFile(url: URL) {
-        let task = URLSession.shared.dataTask(with:url) { (data, response, error) in
-            if error != nil {
-                print(error!)
-            }
-            else {
-                let textFile = String(decoding: data!, as: UTF8.self)
-                // Bug: Can't encode to isoLatin1 so no ÅÄÖ right now
-                self.textToDisplay = textFile
-                // Will be Changed to make it working async
-                NotificationCenter.default.post(name: Notification.Name(rawValue: "downloadComplete"), object: nil)
-            }
-        }
-        task.resume()
     }
 }
-
